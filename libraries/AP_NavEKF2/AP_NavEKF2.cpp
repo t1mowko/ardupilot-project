Jee88/ardupilot

AP_NavEKF2: replace prearm_healthy with pre_arm_check

libraries/AP_NavEKF2/AP_NavEKF2.cpp
@@ -885,13 +885,21 @@ bool NavEKF2::healthy(void) const
     return core[primary].healthy();
 }
 
-bool NavEKF2::all_cores_healthy(void) const
+// returns false if we fail arming checks, in which case the buffer will be populated with a failure message
+bool NavEKF2::pre_arm_check(char *failure_msg, uint8_t failure_msg_len) const
 {
     if (!core) {
+        hal.util->snprintf(failure_msg, failure_msg_len, "no EKF2 cores");
         return false;
     }
     for (uint8_t i = 0; i < num_cores; i++) {
         if (!core[i].healthy()) {
+            const char *failure = core[primary].prearm_failure_reason();
+            if (failure != nullptr) {
+                hal.util->snprintf(failure_msg, failure_msg_len, failure);
+            } else {
+                hal.util->snprintf(failure_msg, failure_msg_len, "EKF2 core %d unhealthy", (int)num_cores);
+            }
             return false;
         }
     }
@@ -1496,21 +1504,6 @@ uint32_t NavEKF2::getLastVelNorthEastReset(Vector2f &vel) const
         return 0;
     }
     return core[primary].getLastVelNorthEastReset(vel);
-}
-
-// report the reason for why the backend is refusing to initialise
-const char *NavEKF2::prearm_failure_reason(void) const
-{
-    if (!core) {
-        return initFailureReason[int(initFailure)];
-    }
-    for (uint8_t i = 0; i < num_cores; i++) {
-        const char * failure = core[i].prearm_failure_reason();
-        if (failure != nullptr) {
-            return failure;
-        }
-    }
-    return nullptr;
 }
 
 // Returns the amount of vertical position change due to the last reset or core switch in metres

t1mowko/ardupilot-project

libraries/AP_NavEKF2/AP_NavEKF2.cpp
@@ -906,13 +906,21 @@ bool NavEKF2::healthy(void) const
     return core[primary].healthy();
 }
 
-bool NavEKF2::all_cores_healthy(void) const
+// returns false if we fail arming checks, in which case the buffer will be populated with a failure message
+bool NavEKF2::pre_arm_check(char *failure_msg, uint8_t failure_msg_len) const
 {
     if (!core) {
+        hal.util->snprintf(failure_msg, failure_msg_len, "no EKF2 cores");
         return false;
     }
     for (uint8_t i = 0; i < num_cores; i++) {
         if (!core[i].healthy()) {
+            const char *failure = core[primary].prearm_failure_reason();
+            if (failure != nullptr) {
+                hal.util->snprintf(failure_msg, failure_msg_len, failure);
+            } else {
+                hal.util->snprintf(failure_msg, failure_msg_len, "EKF2 core %d unhealthy", (int)i);
+            }
             return false;
         }
     }
@@ -1517,21 +1525,6 @@ uint32_t NavEKF2::getLastVelNorthEastReset(Vector2f &vel) const
         return 0;
     }
     return core[primary].getLastVelNorthEastReset(vel);
-}
-
-// report the reason for why the backend is refusing to initialise
-const char *NavEKF2::prearm_failure_reason(void) const
-{
-    if (!core) {
-        return initFailureReason[int(initFailure)];
-    }
-    for (uint8_t i = 0; i < num_cores; i++) {
-        const char * failure = core[i].prearm_failure_reason();
-        if (failure != nullptr) {
-            return failure;
-        }
-    }
-    return nullptr;
 }
 
 // Returns the amount of vertical position change due to the last reset or core switch in metres