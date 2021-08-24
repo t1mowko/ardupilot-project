MattKear/ardupilot

AP_RPM: Restructure params to be per instance of _params

libraries/AP_RPM/AP_RPM_Params.cpp
@@ -1,0 +1,75 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <AP_RPM/AP_RPM_Params.h>
+
+// table of user settable parameters
+const AP_Param::GroupInfo AP_RPM_Params::var_info[] = {
+    // @Param: TYPE
+    // @DisplayName: RPM type
+    // @Description: What type of RPM sensor is connected
+    // @Values: 0:None,1:PWM,2:AUXPIN,3:EFI,4:Harmonic Notch,5:ESC Telemetry Motors Bitmask
+    // @User: Standard
+    AP_GROUPINFO_FLAGS("TYPE",    1, AP_RPM_Params, _type, 0, AP_PARAM_FLAG_ENABLE),
+
+    // @Param: SCALING
+    // @DisplayName: RPM scaling
+    // @Description: Scaling factor between sensor reading and RPM.
+    // @Increment: 0.001
+    // @User: Standard
+    AP_GROUPINFO("SCALING", 2, AP_RPM_Params, _scaling, 1.0f),
+
+    // @Param: _MAX
+    // @DisplayName: Maximum RPM
+    // @Description: Maximum RPM to report. Only used on type = PWM.
+    // @Increment: 1
+    // @User: Standard
+    AP_GROUPINFO("MAX", 3, AP_RPM_Params, _maximum, 100000),
+
+    // @Param: _MIN
+    // @DisplayName: Minimum RPM
+    // @Description: Minimum RPM to report. Only used on type = PWM.
+    // @Increment: 1
+    // @User: Standard
+    AP_GROUPINFO("MIN", 4, AP_RPM_Params, _minimum, 10),
+
+    // @Param: _MIN_QUAL
+    // @DisplayName: Minimum Quality
+    // @Description: Minimum data quality to be used
+    // @Increment: 0.1
+    // @User: Advanced
+    AP_GROUPINFO("MIN_QUAL", 5, AP_RPM_Params, _quality_min, 0.5),
+
+    // @Param: _PIN
+    // @DisplayName: Input pin number
+    // @Description: Which pin to use. Only used on type = PWM.
+    // @Values: -1:Disabled,50:PixhawkAUX1,51:PixhawkAUX2,52:PixhawkAUX3,53:PixhawkAUX4,54:PixhawkAUX5,55:PixhawkAUX6
+    // @User: Standard
+    AP_GROUPINFO("PIN",    6, AP_RPM_Params, _pin, 54),
+
+    // @Param: _ESC_MASK
+    // @DisplayName: Bitmask of ESC telemetry channels to average
+    // @Description: Mask of channels which support ESC rpm telemetry. RPM telemetry of the selected channels will be averaged
+    // @Bitmask: 0:Channel1,1:Channel2,2:Channel3,3:Channel4,4:Channel5,5:Channel6,6:Channel7,7:Channel8,8:Channel9,9:Channel10,10:Channel11,11:Channel12,12:Channel13,13:Channel14,14:Channel15,15:Channel16
+    // @User: Advanced
+    AP_GROUPINFO("ESC_MASK",  7, AP_RPM_Params, _esc_mask, 0),
+
+    AP_GROUPEND
+};
+
+AP_RPM_Params::AP_RPM_Params(void)
+{
+    AP_Param::setup_object_defaults(this, var_info);
+}

libraries/AP_RPM/AP_RPM_Params.h
@@ -12,33 +12,24 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#pragma once
+#include <AP_Param/AP_Param.h>
 
-#include <AP_HAL/AP_HAL.h>
+class AP_RPM_Params {
 
-#include "RPM_EFI.h"
+public:
+    // Constructor
+    AP_RPM_Params(void);
 
-#if HAL_EFI_ENABLED
-extern const AP_HAL::HAL& hal;
+    // parameters for each instance
+    AP_Int8  _type;
+    AP_Int8  _pin;
+    AP_Float _scaling;
+    AP_Float _maximum;
+    AP_Float _minimum;
+    AP_Float _quality_min;
+    AP_Int32 _esc_mask;
 
-/* 
-   open the sensor in constructor
-*/
-AP_RPM_EFI::AP_RPM_EFI(AP_RPM &_ap_rpm, uint8_t _instance, AP_RPM::RPM_State &_state) :
-    AP_RPM_Backend(_ap_rpm, _instance, _state)
-{
-    instance = _instance;
-}
+    static const struct AP_Param::GroupInfo var_info[];
 
-void AP_RPM_EFI::update(void)
-{
-    AP_EFI *efi = AP::EFI();
-    if (efi == nullptr) {
-        return;
-    }
-    state.rate_rpm = efi->get_rpm();
-    state.rate_rpm *= ap_rpm._params[state.instance]._scaling;
-    state.signal_quality = 0.5f;
-    state.last_reading_ms = AP_HAL::millis();
-}
-
-#endif // HAL_EFI_ENABLED
+};

libraries/AP_RPM/RPM_Backend.h
@@ -35,7 +35,7 @@ public:
         if (state.instance >= RPM_MAX_INSTANCES) {
             return -1;
         }
-        return ap_rpm._pin[state.instance].get();
+        return ap_rpm._params[state.instance]._pin.get();
     }
     
 protected:

libraries/AP_RPM/RPM_ESC_Telem.cpp
@@ -34,8 +34,8 @@ void AP_RPM_ESC_Telem::update(void)
 {
 #if HAL_WITH_ESC_TELEM
     AP_ESC_Telem &esc_telem = AP::esc_telem();
-    float esc_rpm = esc_telem.get_average_motor_rpm(ap_rpm._esc_mask[state.instance]);
-    state.rate_rpm = esc_rpm * ap_rpm._scaling[state.instance];
+    float esc_rpm = esc_telem.get_average_motor_rpm(ap_rpm._params[state.instance]._esc_mask);
+    state.rate_rpm = esc_rpm * ap_rpm._params[state.instance]._scaling;
     state.signal_quality = 0.5f;
     state.last_reading_ms = AP_HAL::millis();
 #endif

libraries/AP_RPM/RPM_HarmonicNotch.cpp
@@ -34,7 +34,7 @@ void AP_RPM_HarmonicNotch::update(void)
     AP_InertialSensor& ins = AP::ins();
     if (ins.get_gyro_harmonic_notch_tracking_mode() != HarmonicNotchDynamicMode::Fixed) {
         state.rate_rpm = ins.get_gyro_dynamic_notch_center_freq_hz() * 60.0f;
-        state.rate_rpm *= ap_rpm._scaling[state.instance];
+        state.rate_rpm *= ap_rpm._params[state.instance]._scaling;
         state.signal_quality = 0.5f;
         state.last_reading_ms = AP_HAL::millis();
     }

libraries/AP_RPM/RPM_SITL.cpp
@@ -40,7 +40,7 @@ void AP_RPM_SITL::update(void)
     } else {
         state.rate_rpm = sitl->state.rpm[1];
     }
-    state.rate_rpm *= ap_rpm._scaling[state.instance];
+    state.rate_rpm *= ap_rpm._params[state.instance]._scaling;
     state.signal_quality = 0.5f;
     state.last_reading_ms = AP_HAL::millis();
 

t1mowko/ardupilot-project

libraries/AP_RPM/AP_RPM_Params.cpp
@@ -1,0 +1,75 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <AP_RPM/AP_RPM_Params.h>
+
+// table of user settable parameters
+const AP_Param::GroupInfo AP_RPM_Params::var_info[] = {
+    // @Param: TYPE
+    // @DisplayName: RPM type
+    // @Description: What type of RPM sensor is connected
+    // @Values: 0:None,1:PWM,2:AUXPIN,3:EFI,4:Harmonic Notch,5:ESC Telemetry Motors Bitmask
+    // @User: Standard
+    AP_GROUPINFO_FLAGS("TYPE", 1, AP_RPM_Params, type, 0, AP_PARAM_FLAG_ENABLE),
+
+    // @Param: SCALING
+    // @DisplayName: RPM scaling
+    // @Description: Scaling factor between sensor reading and RPM.
+    // @Increment: 0.001
+    // @User: Standard
+    AP_GROUPINFO("SCALING", 2, AP_RPM_Params, scaling, 1.0f),
+
+    // @Param: MAX
+    // @DisplayName: Maximum RPM
+    // @Description: Maximum RPM to report. Only used on type = PWM.
+    // @Increment: 1
+    // @User: Standard
+    AP_GROUPINFO("MAX", 3, AP_RPM_Params, maximum, 100000),
+
+    // @Param: MIN
+    // @DisplayName: Minimum RPM
+    // @Description: Minimum RPM to report. Only used on type = PWM.
+    // @Increment: 1
+    // @User: Standard
+    AP_GROUPINFO("MIN", 4, AP_RPM_Params, minimum, 10),
+
+    // @Param: MIN_QUAL
+    // @DisplayName: Minimum Quality
+    // @Description: Minimum data quality to be used
+    // @Increment: 0.1
+    // @User: Advanced
+    AP_GROUPINFO("MIN_QUAL", 5, AP_RPM_Params, quality_min, 0.5),
+
+    // @Param: PIN
+    // @DisplayName: Input pin number
+    // @Description: Which pin to use. Only used on type = PWM.
+    // @Values: -1:Disabled,50:AUX1,51:AUX2,52:AUX3,53:AUX4,54:AUX5,55:AUX6
+    // @User: Standard
+    AP_GROUPINFO("PIN", 6, AP_RPM_Params, pin, -1),
+
+    // @Param: ESC_MASK
+    // @DisplayName: Bitmask of ESC telemetry channels to average
+    // @Description: Mask of channels which support ESC rpm telemetry. RPM telemetry of the selected channels will be averaged
+    // @Bitmask: 0:Channel1,1:Channel2,2:Channel3,3:Channel4,4:Channel5,5:Channel6,6:Channel7,7:Channel8,8:Channel9,9:Channel10,10:Channel11,11:Channel12,12:Channel13,13:Channel14,14:Channel15,15:Channel16
+    // @User: Advanced
+    AP_GROUPINFO("ESC_MASK", 7, AP_RPM_Params, esc_mask, 0),
+
+    AP_GROUPEND
+};
+
+AP_RPM_Params::AP_RPM_Params(void)
+{
+    AP_Param::setup_object_defaults(this, var_info);
+}

libraries/AP_RPM/AP_RPM_Params.h
@@ -12,33 +12,24 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#pragma once
+#include <AP_Param/AP_Param.h>
 
-#include <AP_HAL/AP_HAL.h>
+class AP_RPM_Params {
 
-#include "RPM_EFI.h"
+public:
+    // Constructor
+    AP_RPM_Params(void);
 
-#if HAL_EFI_ENABLED
-extern const AP_HAL::HAL& hal;
+    // parameters for each instance
+    AP_Int8  type;
+    AP_Int8  pin;
+    AP_Float scaling;
+    AP_Float maximum;
+    AP_Float minimum;
+    AP_Float quality_min;
+    AP_Int32 esc_mask;
 
-/* 
-   open the sensor in constructor
-*/
-AP_RPM_EFI::AP_RPM_EFI(AP_RPM &_ap_rpm, uint8_t _instance, AP_RPM::RPM_State &_state) :
-    AP_RPM_Backend(_ap_rpm, _instance, _state)
-{
-    instance = _instance;
-}
+    static const struct AP_Param::GroupInfo var_info[];
 
-void AP_RPM_EFI::update(void)
-{
-    AP_EFI *efi = AP::EFI();
-    if (efi == nullptr) {
-        return;
-    }
-    state.rate_rpm = efi->get_rpm();
-    state.rate_rpm *= ap_rpm._params[state.instance].scaling;
-    state.signal_quality = 0.5f;
-    state.last_reading_ms = AP_HAL::millis();
-}
-
-#endif // HAL_EFI_ENABLED
+};

libraries/AP_RPM/RPM_Backend.h
@@ -35,7 +35,7 @@ public:
         if (state.instance >= RPM_MAX_INSTANCES) {
             return -1;
         }
-        return ap_rpm._pin[state.instance].get();
+        return ap_rpm._params[state.instance].pin.get();
     }
     
 protected:

libraries/AP_RPM/RPM_ESC_Telem.cpp
@@ -34,8 +34,8 @@ void AP_RPM_ESC_Telem::update(void)
 {
 #if HAL_WITH_ESC_TELEM
     AP_ESC_Telem &esc_telem = AP::esc_telem();
-    float esc_rpm = esc_telem.get_average_motor_rpm(ap_rpm._esc_mask[state.instance]);
-    state.rate_rpm = esc_rpm * ap_rpm._scaling[state.instance];
+    float esc_rpm = esc_telem.get_average_motor_rpm(ap_rpm._params[state.instance].esc_mask);
+    state.rate_rpm = esc_rpm * ap_rpm._params[state.instance].scaling;
     state.signal_quality = 0.5f;
     state.last_reading_ms = AP_HAL::millis();
 #endif

libraries/AP_RPM/RPM_HarmonicNotch.cpp
@@ -34,7 +34,7 @@ void AP_RPM_HarmonicNotch::update(void)
     AP_InertialSensor& ins = AP::ins();
     if (ins.get_gyro_harmonic_notch_tracking_mode() != HarmonicNotchDynamicMode::Fixed) {
         state.rate_rpm = ins.get_gyro_dynamic_notch_center_freq_hz() * 60.0f;
-        state.rate_rpm *= ap_rpm._scaling[state.instance];
+        state.rate_rpm *= ap_rpm._params[state.instance].scaling;
         state.signal_quality = 0.5f;
         state.last_reading_ms = AP_HAL::millis();
     }

libraries/AP_RPM/RPM_SITL.cpp
@@ -40,7 +40,7 @@ void AP_RPM_SITL::update(void)
     } else {
         state.rate_rpm = sitl->state.rpm[1];
     }
-    state.rate_rpm *= ap_rpm._scaling[state.instance];
+    state.rate_rpm *= ap_rpm._params[state.instance].scaling;
     state.signal_quality = 0.5f;
     state.last_reading_ms = AP_HAL::millis();
 