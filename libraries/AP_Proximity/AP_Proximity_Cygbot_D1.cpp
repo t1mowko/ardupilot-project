rishabsingh3003/ardupilot

AP_Proximity: move boundary to frontend
Co-authored-by: Rishabh <f20171602@hyderabad.bits-pilani.ac.in>

libraries/AP_Proximity/AP_Proximity_Cygbot_D1.cpp
@@ -118,7 +118,7 @@ bool AP_Proximity_Cygbot_D1::parse_byte(uint8_t data)
         // checksum is valid, parse payload
         _last_distance_received_ms = AP_HAL::millis();
         parse_payload();
-        _temp_boundary.update_3D_boundary(boundary);
+        _temp_boundary.update_3D_boundary(frontend.boundary);
         reset();
         return true;
     }
@@ -148,7 +148,7 @@ void AP_Proximity_Cygbot_D1::parse_payload()
                 continue;
             }
             // convert angle to face
-            const AP_Proximity_Boundary_3D::Face face = boundary.get_face(sampled_angle);
+            const AP_Proximity_Boundary_3D::Face face = frontend.boundary.get_face(sampled_angle);
             // push face to temp boundary
             _temp_boundary.add_distance(face, sampled_angle, distance_m);
             // push to OA_DB

t1mowko/ardupilot-project

libraries/AP_Proximity/AP_Proximity_Cygbot_D1.cpp
@@ -118,7 +118,7 @@ bool AP_Proximity_Cygbot_D1::parse_byte(uint8_t data)
         // checksum is valid, parse payload
         _last_distance_received_ms = AP_HAL::millis();
         parse_payload();
-        _temp_boundary.update_3D_boundary(boundary);
+        _temp_boundary.update_3D_boundary(frontend.boundary);
         reset();
         return true;
     }
@@ -149,7 +149,8 @@ void AP_Proximity_Cygbot_D1::parse_payload()
                 continue;
             }
             // convert angle to face
-            const AP_Proximity_Boundary_3D::Face face = boundary.get_face(corrected_angle);
+            const AP_Proximity_Boundary_3D::Face face = frontend.boundary.get_face(corrected_angle);
+
             // push face to temp boundary
             _temp_boundary.add_distance(face, corrected_angle, distance_m);
             // push to OA_DB