rishabsingh3003/ardupilot

AP_RangeFinder: Add TOFSenseP CAN Rangefinder

libraries/AP_RangeFinder/AP_RangeFinder_Backend_CAN.h
@@ -1,0 +1,78 @@
+#pragma once
+
+#include "AP_RangeFinder_Backend.h"
+
+#if HAL_MAX_CAN_PROTOCOL_DRIVERS
+
+#include <AP_CANManager/AP_CANSensor.h>
+#include <AP_BoardConfig/AP_BoardConfig.h>
+
+class RangeFinder_MultiCAN;
+
+class AP_RangeFinder_Backend_CAN : public AP_RangeFinder_Backend
+{
+public:
+    // constructor
+    AP_RangeFinder_Backend_CAN(RangeFinder::RangeFinder_State &_state,
+                                  AP_RangeFinder_Params &_params);
+
+    friend class RangeFinder_MultiCAN;
+
+    static const struct AP_Param::GroupInfo var_info[];
+
+protected:
+
+    // update state
+    virtual void update(void) override;
+
+    // get distance measurement
+    bool get_reading(float &reading_m);
+
+    // it is essential that anyone relying on the base-class update to implement this
+    virtual bool handle_frame(AP_HAL::CANFrame &frame) = 0;
+
+    // maximum time between readings before we change state to NoData:
+    virtual uint16_t read_timeout_ms() const { return 200; }
+
+    virtual MAV_DISTANCE_SENSOR _get_mav_distance_sensor_type() const override {
+        return MAV_DISTANCE_SENSOR_RADAR;
+    }
+
+    // return true if the CAN ID is correct
+    bool is_correct_id(uint32_t can_id) const;
+
+    // set distance and count
+    void set_distance_m(float distance_m) {
+        _distance_sum += distance_m;
+        _distance_count++;
+    };
+
+    // linked list
+    AP_RangeFinder_Backend_CAN *next;
+
+    AP_Int32 receive_id; // CAN ID to receive for this backend
+    AP_Int32 snr_min; // minimum signal strength to accept packet
+
+private:
+
+    float _distance_sum; // meters
+    uint32_t _distance_count;
+};
+
+// a class to allow for multiple CAN backends with one
+// CANSensor driver
+class RangeFinder_MultiCAN : public CANSensor {
+public:
+    RangeFinder_MultiCAN(AP_CANManager::Driver_Type can_type, const char *driver_name) : CANSensor(driver_name) {
+        register_driver(can_type);
+    }
+
+    // handler for incoming frames
+    void handle_frame(AP_HAL::CANFrame &frame) override;
+
+    // Semaphore for access to shared backend data
+    HAL_Semaphore sem;
+    AP_RangeFinder_Backend_CAN *drivers;
+};
+
+#endif // HAL_MAX_CAN_PROTOCOL_DRIVERS

libraries/AP_RangeFinder/AP_RangeFinder_Benewake_CAN.cpp
@@ -5,35 +5,16 @@
 
 #if AP_RANGEFINDER_BENEWAKE_CAN_ENABLED
 
-const AP_Param::GroupInfo AP_RangeFinder_Benewake_CAN::var_info[] = {
-
-    // @Param: RECV_ID
-    // @DisplayName: CAN receive ID
-    // @Description: The receive ID of the CAN frames. A value of zero means all IDs are accepted.
-    // @Range: 0 65535
-    // @User: Advanced
-    AP_GROUPINFO("RECV_ID", 10, AP_RangeFinder_Benewake_CAN, receive_id, 0),
-
-    // @Param: SNR_MIN
-    // @DisplayName: Minimum signal strength
-    // @Description: Minimum signal strength (SNR) to accept distance
-    // @Range: 0 65535
-    // @User: Advanced
-    AP_GROUPINFO("SNR_MIN", 11, AP_RangeFinder_Benewake_CAN, snr_min, 0),
-
-    AP_GROUPEND
-};
-
-Benewake_MultiCAN *AP_RangeFinder_Benewake_CAN::multican;
+RangeFinder_MultiCAN *AP_RangeFinder_Benewake_CAN::multican;
 
 /*
   constructor
  */
 AP_RangeFinder_Benewake_CAN::AP_RangeFinder_Benewake_CAN(RangeFinder::RangeFinder_State &_state, AP_RangeFinder_Params &_params) :
-    AP_RangeFinder_Backend(_state, _params)
+    AP_RangeFinder_Backend_CAN(_state, _params)
 {
     if (multican == nullptr) {
-        multican = new Benewake_MultiCAN();
+        multican = new RangeFinder_MultiCAN(AP_CANManager::Driver_Type::Driver_Type_Benewake, "Benewake MultiCAN");
         if (multican == nullptr) {
             AP_BoardConfig::allocation_error("Benewake_CAN");
         }
@@ -45,26 +26,6 @@ AP_RangeFinder_Benewake_CAN::AP_RangeFinder_Benewake_CAN(RangeFinder::RangeFinde
         auto *prev = multican->drivers;
         next = prev;
         multican->drivers = this;
-    }
-
-    AP_Param::setup_object_defaults(this, var_info);
-    state.var_info = var_info;
-}
-
-// update state
-void AP_RangeFinder_Benewake_CAN::update(void)
-{
-    WITH_SEMAPHORE(_sem);
-    const uint32_t now = AP_HAL::millis();
-    if (_distance_count == 0 && now - state.last_reading_ms > 500) {
-        // no new data.
-        set_status(RangeFinder::Status::NoData);
-    } else if (_distance_count != 0) {
-        state.distance_m = 0.01 * (_distance_sum_cm / _distance_count);
-        state.last_reading_ms = AP_HAL::millis();
-        _distance_sum_cm = 0;
-        _distance_count = 0;
-        update_status();
     }
 }
 
@@ -83,8 +44,7 @@ bool AP_RangeFinder_Benewake_CAN::handle_frame_H30(AP_HAL::CANFrame &frame)
     //uint16_t target2 = be16toh_ptr(&frame.data[2]);
     //uint16_t target3 = be16toh_ptr(&frame.data[4]);
 
-    _distance_sum_cm += target1_cm;
-    _distance_count++;
+    set_distance_m(target1_cm * 0.01);
 
     return true;
 }
@@ -96,28 +56,16 @@ bool AP_RangeFinder_Benewake_CAN::handle_frame(AP_HAL::CANFrame &frame)
     if (frame.isExtended()) {
         // H30 radar uses extended frames
         const int32_t id = int32_t(frame.id & AP_HAL::CANFrame::MaskExtID);
-        if (receive_id != 0 && id != receive_id.get()) {
-            // incorrect receive ID
+        if (!is_correct_id(id)) {
             return false;
         }
-        if (last_recv_id != -1 && id != last_recv_id) {
-            // changing ID
-            return false;
-        }
-        last_recv_id = id;
         return handle_frame_H30(frame);
     }
 
     const uint16_t id = frame.id & AP_HAL::CANFrame::MaskStdID;
-    if (receive_id != 0 && id != uint16_t(receive_id.get())) {
-        // incorrect receive ID
+    if (!is_correct_id(id)) {
         return false;
     }
-    if (last_recv_id != -1 && id != last_recv_id) {
-        // changing ID
-        return false;
-    }
-    last_recv_id = id;
 
     const uint16_t dist_cm = le16toh_ptr(&frame.data[0]);
     const uint16_t snr = le16toh_ptr(&frame.data[2]);
@@ -125,20 +73,9 @@ bool AP_RangeFinder_Benewake_CAN::handle_frame(AP_HAL::CANFrame &frame)
         // too low signal strength
         return true;
     }
-    _distance_sum_cm += dist_cm;
-    _distance_count++;
-    return true;
-}
 
-// handle frames from CANSensor, passing to the drivers
-void Benewake_MultiCAN::handle_frame(AP_HAL::CANFrame &frame)
-{
-    WITH_SEMAPHORE(sem);
-    for (auto *d = drivers; d; d=d->next) {
-        if (d->handle_frame(frame)) {
-            break;
-        }
-    }
+    set_distance_m(dist_cm * 0.01);
+    return true;
 }
 
 #endif  // AP_RANGEFINDER_BENEWAKE_CAN_ENABLED

libraries/AP_RangeFinder/AP_RangeFinder_TOFSenseP_CAN.cpp
@@ -1,0 +1,58 @@
+#include "AP_RangeFinder_TOFSenseP_CAN.h"
+#include <GCS_MAVLink/GCS.h>
+#include <AP_BoardConfig/AP_BoardConfig.h>
+#include <AP_HAL/utility/sparse-endian.h>
+
+#if AP_RANGEFINDER_TOFSENSEP_CAN_ENABLED
+
+#include <AP_HAL/AP_HAL.h>
+
+RangeFinder_MultiCAN *AP_RangeFinder_TOFSenseP_CAN::multican_TOFSenseP;
+
+/*
+  constructor
+ */
+AP_RangeFinder_TOFSenseP_CAN::AP_RangeFinder_TOFSenseP_CAN(RangeFinder::RangeFinder_State &_state, AP_RangeFinder_Params &_params) :
+    AP_RangeFinder_Backend_CAN(_state, _params)
+{
+    if (multican_TOFSenseP == nullptr) {
+        multican_TOFSenseP = new RangeFinder_MultiCAN(AP_CANManager::Driver_Type::Driver_Type_NoopLoop, "TOFSenseP MultiCAN");
+        if (multican_TOFSenseP == nullptr) {
+            AP_BoardConfig::allocation_error("Rangefinder_MultiCAN");
+        }
+    }
+
+    {
+        // add to linked list of drivers
+        WITH_SEMAPHORE(multican_TOFSenseP->sem);
+        auto *prev = multican_TOFSenseP->drivers;
+        next = prev;
+        multican_TOFSenseP->drivers = this;
+    }
+}
+
+
+// handler for incoming frames. These come in at 10-30Hz
+bool AP_RangeFinder_TOFSenseP_CAN::handle_frame(AP_HAL::CANFrame &frame)
+{
+    WITH_SEMAPHORE(_sem);
+    const uint32_t id = frame.id - 0x200U;
+
+    if (!is_correct_id(id)) {
+        return false;
+    }
+
+    const int32_t dist_cm = (int32_t)(frame.data[0] << 8U | frame.data[1] << 16U | frame.data[2] << 24U) / 2560;
+    const uint8_t status = frame.data[3];
+    const uint16_t snr = le16toh_ptr(&frame.data[4]);
+
+    if ((snr_min != 0 && snr < uint16_t(snr_min.get())) || status > 0) {
+        // too low signal strength or bad status
+        return false;
+    }
+
+    set_distance_m(dist_cm * 0.01);
+    return true;
+}
+
+#endif  // AP_RANGEFINDER_TOFSenseP_CAN_ENABLED

libraries/AP_RangeFinder/AP_RangeFinder_USD1_CAN.cpp
@@ -1,32 +1,19 @@
 #include "AP_RangeFinder_USD1_CAN.h"
-#include <AP_BoardConfig/AP_BoardConfig.h>
 
 #if AP_RANGEFINDER_USD1_CAN_ENABLED
 
 #include <AP_HAL/AP_HAL.h>
 
-const AP_Param::GroupInfo AP_RangeFinder_USD1_CAN::var_info[] = {
-
-    // @Param: RECV_ID
-    // @DisplayName: CAN receive ID
-    // @Description: The receive ID of the CAN frames. A value of zero means all IDs are accepted.
-    // @Range: 0 65535
-    // @User: Advanced
-    AP_GROUPINFO("RECV_ID", 12, AP_RangeFinder_USD1_CAN, receive_id, 0),
-
-    AP_GROUPEND
-};
-
-USD1_MultiCAN *AP_RangeFinder_USD1_CAN::multican;
+RangeFinder_MultiCAN *AP_RangeFinder_USD1_CAN::multican;
 
 /*
   constructor
  */
 AP_RangeFinder_USD1_CAN::AP_RangeFinder_USD1_CAN(RangeFinder::RangeFinder_State &_state, AP_RangeFinder_Params &_params) :
-    AP_RangeFinder_Backend(_state, _params)
+    AP_RangeFinder_Backend_CAN(_state, _params)
 {
     if (multican == nullptr) {
-        multican = new USD1_MultiCAN();
+        multican = new RangeFinder_MultiCAN(AP_CANManager::Driver_Type::Driver_Type_USD1, "USD1 MultiCAN");
         if (multican == nullptr) {
             AP_BoardConfig::allocation_error("USD1_CAN");
         }
@@ -39,26 +26,6 @@ AP_RangeFinder_USD1_CAN::AP_RangeFinder_USD1_CAN(RangeFinder::RangeFinder_State 
         next = prev;
         multican->drivers = this;
     }
-
-    AP_Param::setup_object_defaults(this, var_info);
-    state.var_info = var_info;
-}
-
-// update state
-void AP_RangeFinder_USD1_CAN::update(void)
-{
-    WITH_SEMAPHORE(_sem);
-    const uint32_t now = AP_HAL::millis();
-    if (_distance_count == 0 && now - state.last_reading_ms > 500) {
-        // no new data.
-        set_status(RangeFinder::Status::NoData);
-    } else if (_distance_count != 0) {
-        state.distance_m = _distance_sum / _distance_count;
-        state.last_reading_ms = AP_HAL::millis();
-        _distance_sum = 0;
-        _distance_count = 0;
-        update_status();
-    }
 }
 
 // handler for incoming frames. These come in at 100Hz
@@ -66,31 +33,14 @@ bool AP_RangeFinder_USD1_CAN::handle_frame(AP_HAL::CANFrame &frame)
 {
     WITH_SEMAPHORE(_sem);
     const uint16_t id = frame.id & AP_HAL::CANFrame::MaskStdID;
-    if (receive_id != 0 && id != uint16_t(receive_id.get())) {
-        // incorrect receive ID
+
+    if (!is_correct_id(id)) {
         return false;
     }
-    if (last_recv_id != -1 && id != last_recv_id) {
-        // changing ID
-        return false;
-    }
-    last_recv_id = id;
 
     const uint16_t dist_cm = (frame.data[0]<<8) | frame.data[1];
-    _distance_sum += dist_cm * 0.01;
-    _distance_count++;
+    set_distance_m(dist_cm * 0.01);
     return true;
-}
-
-// handle frames from CANSensor, passing to the drivers
-void USD1_MultiCAN::handle_frame(AP_HAL::CANFrame &frame)
-{
-    WITH_SEMAPHORE(sem);
-    for (auto *d = drivers; d; d=d->next) {
-        if (d->handle_frame(frame)) {
-            break;
-        }
-    }
 }
 
 #endif  // AP_RANGEFINDER_USD1_CAN_ENABLED

t1mowko/ardupilot-project

libraries/AP_RangeFinder/AP_RangeFinder_Backend_CAN.h
@@ -1,0 +1,78 @@
+#pragma once
+
+#include "AP_RangeFinder_Backend.h"
+
+#if HAL_MAX_CAN_PROTOCOL_DRIVERS
+
+#include <AP_CANManager/AP_CANSensor.h>
+#include <AP_BoardConfig/AP_BoardConfig.h>
+
+class RangeFinder_MultiCAN;
+
+class AP_RangeFinder_Backend_CAN : public AP_RangeFinder_Backend
+{
+public:
+    // constructor
+    AP_RangeFinder_Backend_CAN(RangeFinder::RangeFinder_State &_state,
+                                  AP_RangeFinder_Params &_params);
+
+    friend class RangeFinder_MultiCAN;
+
+    static const struct AP_Param::GroupInfo var_info[];
+
+protected:
+
+    // update state
+    virtual void update(void) override;
+
+    // get distance measurement
+    bool get_reading(float &reading_m);
+
+    // it is essential that anyone relying on the base-class update to implement this
+    virtual bool handle_frame(AP_HAL::CANFrame &frame) = 0;
+
+    // maximum time between readings before we change state to NoData:
+    virtual uint16_t read_timeout_ms() const { return 200; }
+
+    virtual MAV_DISTANCE_SENSOR _get_mav_distance_sensor_type() const override {
+        return MAV_DISTANCE_SENSOR_RADAR;
+    }
+
+    // return true if the CAN ID is correct
+    bool is_correct_id(uint32_t can_id) const;
+
+    // set distance and count
+    void set_distance_m(float distance_m) {
+        _distance_sum += distance_m;
+        _distance_count++;
+    };
+
+    // linked list
+    AP_RangeFinder_Backend_CAN *next;
+
+    AP_Int32 receive_id; // CAN ID to receive for this backend
+    AP_Int32 snr_min; // minimum signal strength to accept packet
+
+private:
+
+    float _distance_sum; // meters
+    uint32_t _distance_count;
+};
+
+// a class to allow for multiple CAN backends with one
+// CANSensor driver
+class RangeFinder_MultiCAN : public CANSensor {
+public:
+    RangeFinder_MultiCAN(AP_CAN::Protocol can_type, const char *driver_name) : CANSensor(driver_name) {
+        register_driver(can_type);
+    }
+
+    // handler for incoming frames
+    void handle_frame(AP_HAL::CANFrame &frame) override;
+
+    // Semaphore for access to shared backend data
+    HAL_Semaphore sem;
+    AP_RangeFinder_Backend_CAN *drivers;
+};
+
+#endif // HAL_MAX_CAN_PROTOCOL_DRIVERS

libraries/AP_RangeFinder/AP_RangeFinder_Benewake_CAN.cpp
@@ -5,35 +5,16 @@
 
 #if AP_RANGEFINDER_BENEWAKE_CAN_ENABLED
 
-const AP_Param::GroupInfo AP_RangeFinder_Benewake_CAN::var_info[] = {
-
-    // @Param: RECV_ID
-    // @DisplayName: CAN receive ID
-    // @Description: The receive ID of the CAN frames. A value of zero means all IDs are accepted.
-    // @Range: 0 65535
-    // @User: Advanced
-    AP_GROUPINFO("RECV_ID", 10, AP_RangeFinder_Benewake_CAN, receive_id, 0),
-
-    // @Param: SNR_MIN
-    // @DisplayName: Minimum signal strength
-    // @Description: Minimum signal strength (SNR) to accept distance
-    // @Range: 0 65535
-    // @User: Advanced
-    AP_GROUPINFO("SNR_MIN", 11, AP_RangeFinder_Benewake_CAN, snr_min, 0),
-
-    AP_GROUPEND
-};
-
-Benewake_MultiCAN *AP_RangeFinder_Benewake_CAN::multican;
+RangeFinder_MultiCAN *AP_RangeFinder_Benewake_CAN::multican;
 
 /*
   constructor
  */
 AP_RangeFinder_Benewake_CAN::AP_RangeFinder_Benewake_CAN(RangeFinder::RangeFinder_State &_state, AP_RangeFinder_Params &_params) :
-    AP_RangeFinder_Backend(_state, _params)
+    AP_RangeFinder_Backend_CAN(_state, _params)
 {
     if (multican == nullptr) {
-        multican = new Benewake_MultiCAN();
+        multican = new RangeFinder_MultiCAN(AP_CAN::Protocol::Benewake, "Benewake MultiCAN");
         if (multican == nullptr) {
             AP_BoardConfig::allocation_error("Benewake_CAN");
         }
@@ -45,26 +26,6 @@ AP_RangeFinder_Benewake_CAN::AP_RangeFinder_Benewake_CAN(RangeFinder::RangeFinde
         auto *prev = multican->drivers;
         next = prev;
         multican->drivers = this;
-    }
-
-    AP_Param::setup_object_defaults(this, var_info);
-    state.var_info = var_info;
-}
-
-// update state
-void AP_RangeFinder_Benewake_CAN::update(void)
-{
-    WITH_SEMAPHORE(_sem);
-    const uint32_t now = AP_HAL::millis();
-    if (_distance_count == 0 && now - state.last_reading_ms > 500) {
-        // no new data.
-        set_status(RangeFinder::Status::NoData);
-    } else if (_distance_count != 0) {
-        state.distance_m = 0.01 * (_distance_sum_cm / _distance_count);
-        state.last_reading_ms = AP_HAL::millis();
-        _distance_sum_cm = 0;
-        _distance_count = 0;
-        update_status();
     }
 }
 
@@ -83,8 +44,7 @@ bool AP_RangeFinder_Benewake_CAN::handle_frame_H30(AP_HAL::CANFrame &frame)
     //uint16_t target2 = be16toh_ptr(&frame.data[2]);
     //uint16_t target3 = be16toh_ptr(&frame.data[4]);
 
-    _distance_sum_cm += target1_cm;
-    _distance_count++;
+    set_distance_m(target1_cm * 0.01);
 
     return true;
 }
@@ -96,28 +56,16 @@ bool AP_RangeFinder_Benewake_CAN::handle_frame(AP_HAL::CANFrame &frame)
     if (frame.isExtended()) {
         // H30 radar uses extended frames
         const int32_t id = int32_t(frame.id & AP_HAL::CANFrame::MaskExtID);
-        if (receive_id != 0 && id != receive_id.get()) {
-            // incorrect receive ID
+        if (!is_correct_id(id)) {
             return false;
         }
-        if (last_recv_id != -1 && id != last_recv_id) {
-            // changing ID
-            return false;
-        }
-        last_recv_id = id;
         return handle_frame_H30(frame);
     }
 
     const uint16_t id = frame.id & AP_HAL::CANFrame::MaskStdID;
-    if (receive_id != 0 && id != uint16_t(receive_id.get())) {
-        // incorrect receive ID
+    if (!is_correct_id(id)) {
         return false;
     }
-    if (last_recv_id != -1 && id != last_recv_id) {
-        // changing ID
-        return false;
-    }
-    last_recv_id = id;
 
     const uint16_t dist_cm = le16toh_ptr(&frame.data[0]);
     const uint16_t snr = le16toh_ptr(&frame.data[2]);
@@ -125,20 +73,9 @@ bool AP_RangeFinder_Benewake_CAN::handle_frame(AP_HAL::CANFrame &frame)
         // too low signal strength
         return true;
     }
-    _distance_sum_cm += dist_cm;
-    _distance_count++;
-    return true;
-}
 
-// handle frames from CANSensor, passing to the drivers
-void Benewake_MultiCAN::handle_frame(AP_HAL::CANFrame &frame)
-{
-    WITH_SEMAPHORE(sem);
-    for (auto *d = drivers; d; d=d->next) {
-        if (d->handle_frame(frame)) {
-            break;
-        }
-    }
+    set_distance_m(dist_cm * 0.01);
+    return true;
 }
 
 #endif  // AP_RANGEFINDER_BENEWAKE_CAN_ENABLED

libraries/AP_RangeFinder/AP_RangeFinder_TOFSenseP_CAN.cpp
@@ -1,0 +1,58 @@
+#include "AP_RangeFinder_TOFSenseP_CAN.h"
+#include <GCS_MAVLink/GCS.h>
+#include <AP_BoardConfig/AP_BoardConfig.h>
+#include <AP_HAL/utility/sparse-endian.h>
+
+#if AP_RANGEFINDER_TOFSENSEP_CAN_ENABLED
+
+#include <AP_HAL/AP_HAL.h>
+
+RangeFinder_MultiCAN *AP_RangeFinder_TOFSenseP_CAN::multican_TOFSenseP;
+
+/*
+  constructor
+ */
+AP_RangeFinder_TOFSenseP_CAN::AP_RangeFinder_TOFSenseP_CAN(RangeFinder::RangeFinder_State &_state, AP_RangeFinder_Params &_params) :
+    AP_RangeFinder_Backend_CAN(_state, _params)
+{
+    if (multican_TOFSenseP == nullptr) {
+        multican_TOFSenseP = new RangeFinder_MultiCAN(AP_CAN::Protocol::TOFSenseP, "TOFSenseP MultiCAN");
+        if (multican_TOFSenseP == nullptr) {
+            AP_BoardConfig::allocation_error("Rangefinder_MultiCAN");
+        }
+    }
+
+    {
+        // add to linked list of drivers
+        WITH_SEMAPHORE(multican_TOFSenseP->sem);
+        auto *prev = multican_TOFSenseP->drivers;
+        next = prev;
+        multican_TOFSenseP->drivers = this;
+    }
+}
+
+
+// handler for incoming frames. These come in at 10-30Hz
+bool AP_RangeFinder_TOFSenseP_CAN::handle_frame(AP_HAL::CANFrame &frame)
+{
+    WITH_SEMAPHORE(_sem);
+    const uint32_t id = frame.id - 0x200U;
+
+    if (!is_correct_id(id)) {
+        return false;
+    }
+
+    const int32_t dist_cm = (int32_t)(frame.data[0] << 8U | frame.data[1] << 16U | frame.data[2] << 24U) / 2560;
+    const uint8_t status = frame.data[3];
+    const uint16_t snr = le16toh_ptr(&frame.data[4]);
+
+    if ((snr_min != 0 && snr < uint16_t(snr_min.get())) || status > 0) {
+        // too low signal strength or bad status
+        return false;
+    }
+
+    set_distance_m(dist_cm * 0.01);
+    return true;
+}
+
+#endif  // AP_RANGEFINDER_TOFSenseP_CAN_ENABLED

libraries/AP_RangeFinder/AP_RangeFinder_USD1_CAN.cpp
@@ -1,32 +1,19 @@
 #include "AP_RangeFinder_USD1_CAN.h"
-#include <AP_BoardConfig/AP_BoardConfig.h>
 
 #if AP_RANGEFINDER_USD1_CAN_ENABLED
 
 #include <AP_HAL/AP_HAL.h>
 
-const AP_Param::GroupInfo AP_RangeFinder_USD1_CAN::var_info[] = {
-
-    // @Param: RECV_ID
-    // @DisplayName: CAN receive ID
-    // @Description: The receive ID of the CAN frames. A value of zero means all IDs are accepted.
-    // @Range: 0 65535
-    // @User: Advanced
-    AP_GROUPINFO("RECV_ID", 12, AP_RangeFinder_USD1_CAN, receive_id, 0),
-
-    AP_GROUPEND
-};
-
-USD1_MultiCAN *AP_RangeFinder_USD1_CAN::multican;
+RangeFinder_MultiCAN *AP_RangeFinder_USD1_CAN::multican;
 
 /*
   constructor
  */
 AP_RangeFinder_USD1_CAN::AP_RangeFinder_USD1_CAN(RangeFinder::RangeFinder_State &_state, AP_RangeFinder_Params &_params) :
-    AP_RangeFinder_Backend(_state, _params)
+    AP_RangeFinder_Backend_CAN(_state, _params)
 {
     if (multican == nullptr) {
-        multican = new USD1_MultiCAN();
+        multican = new RangeFinder_MultiCAN(AP_CAN::Protocol::USD1, "USD1 MultiCAN");
         if (multican == nullptr) {
             AP_BoardConfig::allocation_error("USD1_CAN");
         }
@@ -39,26 +26,6 @@ AP_RangeFinder_USD1_CAN::AP_RangeFinder_USD1_CAN(RangeFinder::RangeFinder_State 
         next = prev;
         multican->drivers = this;
     }
-
-    AP_Param::setup_object_defaults(this, var_info);
-    state.var_info = var_info;
-}
-
-// update state
-void AP_RangeFinder_USD1_CAN::update(void)
-{
-    WITH_SEMAPHORE(_sem);
-    const uint32_t now = AP_HAL::millis();
-    if (_distance_count == 0 && now - state.last_reading_ms > 500) {
-        // no new data.
-        set_status(RangeFinder::Status::NoData);
-    } else if (_distance_count != 0) {
-        state.distance_m = _distance_sum / _distance_count;
-        state.last_reading_ms = AP_HAL::millis();
-        _distance_sum = 0;
-        _distance_count = 0;
-        update_status();
-    }
 }
 
 // handler for incoming frames. These come in at 100Hz
@@ -66,31 +33,14 @@ bool AP_RangeFinder_USD1_CAN::handle_frame(AP_HAL::CANFrame &frame)
 {
     WITH_SEMAPHORE(_sem);
     const uint16_t id = frame.id & AP_HAL::CANFrame::MaskStdID;
-    if (receive_id != 0 && id != uint16_t(receive_id.get())) {
-        // incorrect receive ID
+
+    if (!is_correct_id(id)) {
         return false;
     }
-    if (last_recv_id != -1 && id != last_recv_id) {
-        // changing ID
-        return false;
-    }
-    last_recv_id = id;
 
     const uint16_t dist_cm = (frame.data[0]<<8) | frame.data[1];
-    _distance_sum += dist_cm * 0.01;
-    _distance_count++;
+    set_distance_m(dist_cm * 0.01);
     return true;
-}
-
-// handle frames from CANSensor, passing to the drivers
-void USD1_MultiCAN::handle_frame(AP_HAL::CANFrame &frame)
-{
-    WITH_SEMAPHORE(sem);
-    for (auto *d = drivers; d; d=d->next) {
-        if (d->handle_frame(frame)) {
-            break;
-        }
-    }
 }
 
 #endif  // AP_RANGEFINDER_USD1_CAN_ENABLED