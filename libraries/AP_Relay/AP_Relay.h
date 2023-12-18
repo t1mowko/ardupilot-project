SpektreWorks/ardupilot

AP_Relay: Refactor to support RELAYx_FUNCTION

libraries/AP_Relay/AP_Relay.h
@@ -10,6 +10,8 @@
 #pragma once
 
 #include <AP_Param/AP_Param.h>
+#include <AP_Relay/AP_Relay_Params.h>
+#include <AP_Common/Bitmask.h>
 
 #define AP_RELAY_NUM_RELAYS 6
 
@@ -32,12 +34,10 @@ public:
     void        off(uint8_t instance) { set(instance, false); }
 
     // get state of relay
-    uint8_t     get(uint8_t instance) const {
-        return instance < AP_RELAY_NUM_RELAYS ? _pin_states & (1U<<instance) : 0;
-    }
+    bool        get(uint8_t instance) const;
     
     // see if the relay is enabled
-    bool        enabled(uint8_t instance) { return instance < AP_RELAY_NUM_RELAYS && _pin[instance] != -1; }
+    bool        enabled(uint8_t instance) const { return instance < AP_RELAY_NUM_RELAYS && _params[instance].pin != -1; }
 
     // toggle the relay status
     void        toggle(uint8_t instance);
@@ -49,16 +49,21 @@ public:
 
     static const struct AP_Param::GroupInfo        var_info[];
 
+    bool send_relay_status(const class GCS_MAVLINK &link) const;
+
+    void set(AP_Relay_Params::Function function, bool value);
+
 private:
     static AP_Relay *singleton;
 
-    AP_Int8 _pin[AP_RELAY_NUM_RELAYS];
-    AP_Int8 _default;
-    uint8_t _pin_states;
-    uint8_t _last_logged_pin_states;
-    uint32_t _last_log_ms;
+    AP_Relay_Params _params[AP_RELAY_NUM_RELAYS];
 
     void set(uint8_t instance, bool value);
+
+    void set_defaults();
+    void convert_params();
+
+    void set_pin_by_instance (uint8_t instance, bool value);
 };
 
 namespace AP {

libraries/AP_Relay/AP_Relay_Params.cpp
@@ -1,0 +1,32 @@
+#include <AP_Common/AP_Common.h>
+#include "AP_Relay_Params.h"
+
+const AP_Param::GroupInfo AP_Relay_Params::var_info[] = {
+    // @Param: FUNCTION
+    // @DisplayName: Relay function
+    // @Description: The function the relay channel is mapped to.
+    // @Values: 0:None,1:Relay,2:Ignition,3:Starter
+    // @User: Standard
+    AP_GROUPINFO_FLAGS("FUNCTION", 1, AP_Relay_Params, function, (float)Function::none, AP_PARAM_FLAG_ENABLE),
+
+    // @Param: PIN
+    // @DisplayName: Relay pin
+    // @Description: Digital pin number for relay control. Some common values are given, but see the Wiki's "GPIOs" page for how to determine the pin number for a given autopilot.
+    // @Values: -1:Disabled,49:BB Blue GP0 pin 4,50:AUXOUT1,51:AUXOUT2,52:AUXOUT3,53:AUXOUT4,54:AUXOUT5,55:AUXOUT6,57:BB Blue GP0 pin 3,113:BB Blue GP0 pin 6,116:BB Blue GP0 pin 5,62:BBBMini Pin P8.13,101:MainOut1,102:MainOut2,103:MainOut3,104:MainOut4,105:MainOut5,106:MainOut6,107:MainOut7,108:MainOut8
+    // @User: Standard
+    AP_GROUPINFO("PIN", 2, AP_Relay_Params, pin, -1),
+    
+    // @Param: DEFAULT
+    // @DisplayName: Relay default state
+    // @Description: Should the relay default to on or off, this only applies to function Relay. All other uses will pick the appropriate default output state.
+    // @Values: 0: Off,1:On,2:NoChange
+    // @User: Standard
+    AP_GROUPINFO("DEFAULT", 3, AP_Relay_Params, default_state, (float)Default_State::Off),
+
+    AP_GROUPEND
+
+};
+
+AP_Relay_Params::AP_Relay_Params(void) {
+    AP_Param::setup_object_defaults(this, var_info);
+}

t1mowko/ardupilot-project

libraries/AP_Relay/AP_Relay.h
@@ -14,6 +14,8 @@
 #if AP_RELAY_ENABLED
 
 #include <AP_Param/AP_Param.h>
+#include <AP_Relay/AP_Relay_Params.h>
+#include <AP_Common/Bitmask.h>
 
 #define AP_RELAY_NUM_RELAYS 6
 
@@ -29,9 +31,6 @@ public:
     // setup the relay pin
     void        init();
 
-    // set relay to state
-    void set(uint8_t instance, bool value);
-
     // activate the relay
     void        on(uint8_t instance) { set(instance, true); }
 
@@ -39,12 +38,10 @@ public:
     void        off(uint8_t instance) { set(instance, false); }
 
     // get state of relay
-    uint8_t     get(uint8_t instance) const {
-        return instance < AP_RELAY_NUM_RELAYS ? _pin_states & (1U<<instance) : 0;
-    }
+    bool        get(uint8_t instance) const;
     
     // see if the relay is enabled
-    bool        enabled(uint8_t instance) const { return instance < AP_RELAY_NUM_RELAYS && _pin[instance] != -1; }
+    bool        enabled(uint8_t instance) const;
 
     // toggle the relay status
     void        toggle(uint8_t instance);
@@ -58,14 +55,19 @@ public:
 
     bool send_relay_status(const class GCS_MAVLINK &link) const;
 
+    void set(AP_Relay_Params::Function function, bool value);
+
 private:
     static AP_Relay *singleton;
 
-    AP_Int8 _pin[AP_RELAY_NUM_RELAYS];
-    AP_Int8 _default;
-    uint8_t _pin_states;
-    uint8_t _last_logged_pin_states;
-    uint32_t _last_log_ms;
+    AP_Relay_Params _params[AP_RELAY_NUM_RELAYS];
+
+    void set(uint8_t instance, bool value);
+
+    void set_defaults();
+    void convert_params();
+
+    void set_pin_by_instance(uint8_t instance, bool value);
 };
 
 namespace AP {

libraries/AP_Relay/AP_Relay_Params.cpp
@@ -1,0 +1,32 @@
+#include <AP_Common/AP_Common.h>
+#include "AP_Relay_Params.h"
+
+const AP_Param::GroupInfo AP_Relay_Params::var_info[] = {
+    // @Param: FUNCTION
+    // @DisplayName: Relay function
+    // @Description: The function the relay channel is mapped to.
+    // @Values: 0:None,1:Relay,2:Ignition,3:Starter
+    // @User: Standard
+    AP_GROUPINFO_FLAGS("FUNCTION", 1, AP_Relay_Params, function, (float)Function::none, AP_PARAM_FLAG_ENABLE),
+
+    // @Param: PIN
+    // @DisplayName: Relay pin
+    // @Description: Digital pin number for relay control. Some common values are given, but see the Wiki's "GPIOs" page for how to determine the pin number for a given autopilot.
+    // @Values: -1:Disabled,49:BB Blue GP0 pin 4,50:AUXOUT1,51:AUXOUT2,52:AUXOUT3,53:AUXOUT4,54:AUXOUT5,55:AUXOUT6,57:BB Blue GP0 pin 3,113:BB Blue GP0 pin 6,116:BB Blue GP0 pin 5,62:BBBMini Pin P8.13,101:MainOut1,102:MainOut2,103:MainOut3,104:MainOut4,105:MainOut5,106:MainOut6,107:MainOut7,108:MainOut8
+    // @User: Standard
+    AP_GROUPINFO("PIN", 2, AP_Relay_Params, pin, -1),
+    
+    // @Param: DEFAULT
+    // @DisplayName: Relay default state
+    // @Description: Should the relay default to on or off, this only applies to function Relay. All other uses will pick the appropriate default output state.
+    // @Values: 0: Off,1:On,2:NoChange
+    // @User: Standard
+    AP_GROUPINFO("DEFAULT", 3, AP_Relay_Params, default_state, (float)DefaultState::OFF),
+
+    AP_GROUPEND
+
+};
+
+AP_Relay_Params::AP_Relay_Params(void) {
+    AP_Param::setup_object_defaults(this, var_info);
+}