peterbarker/ardupilot

AP_Logger: disable HAL_LOGGING_BLOCK_ENABLED if not HAL_LOGGING_ENABLED
HAL_LOGGING_DATAFLASH_ENABLED is set unconditionally in some hwdefs, but we still need to honour HAL_LOGGING_ENABLED

libraries/AP_Logger/AP_Logger_config.h
@@ -26,9 +26,17 @@
 #endif
 
 #if HAL_LOGGING_DATAFLASH_ENABLED
-    #define HAL_LOGGING_BLOCK_ENABLED 1
+    #define HAL_LOGGING_BLOCK_ENABLED HAL_LOGGING_ENABLED
 #else
     #define HAL_LOGGING_BLOCK_ENABLED 0
+#endif
+
+#ifndef HAL_LOGGING_W25N01GV_ENABLED
+#define HAL_LOGGING_W25N01GV_ENABLED HAL_LOGGING_BLOCK_ENABLED
+#endif
+
+#ifndef HAL_LOGGING_JEDEC_ENABLED
+#define HAL_LOGGING_JEDEC_ENABLED HAL_LOGGING_BLOCK_ENABLED
 #endif
 
 #if HAL_LOGGING_FILESYSTEM_ENABLED

t1mowko/ardupilot-project

libraries/AP_Logger/AP_Logger_config.h
@@ -26,9 +26,17 @@
 #endif
 
 #if HAL_LOGGING_DATAFLASH_ENABLED
-    #define HAL_LOGGING_BLOCK_ENABLED 1
+    #define HAL_LOGGING_BLOCK_ENABLED HAL_LOGGING_ENABLED
 #else
     #define HAL_LOGGING_BLOCK_ENABLED 0
+#endif
+
+#ifndef HAL_LOGGING_FLASH_W25NXX_ENABLED
+#define HAL_LOGGING_FLASH_W25NXX_ENABLED HAL_LOGGING_BLOCK_ENABLED
+#endif
+
+#ifndef HAL_LOGGING_FLASH_JEDEC_ENABLED
+#define HAL_LOGGING_FLASH_JEDEC_ENABLED HAL_LOGGING_BLOCK_ENABLED
 #endif
 
 #if HAL_LOGGING_FILESYSTEM_ENABLED