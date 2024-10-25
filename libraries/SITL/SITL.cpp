tompoch2/ardupilot

SITL: FlightAxis: add options bitmask parameter

libraries/SITL/SITL.cpp
@@ -39,6 +39,7 @@
 
 #include "SIM_StratoBlimp.h"
 #include "SIM_Glider.h"
+#include "SIM_FlightAxis.h"
 
 extern const AP_HAL::HAL& hal;
 
@@ -1485,6 +1486,12 @@ const AP_Param::GroupInfo SIM::ModelParm::var_info[] = {
     // @Group: SLUP_
     // @Path: ./SIM_SlungPayload.cpp
     AP_SUBGROUPINFO(slung_payload_sim, "SLUP_", 4, SIM::ModelParm, SlungPayloadSim),
+#endif
+
+#if AP_SIM_FLIGHTAXIS_ENABLED
+    // @Group: FTAX_
+    // @Path: ./SIM_FlightAxis.cpp
+    AP_SUBGROUPPTR(flightaxis_ptr, "FTAX_", 5, SIM::ModelParm, FlightAxis),
 #endif
 
     AP_GROUPEND

t1mowko/ardupilot-project

libraries/SITL/SITL.cpp
@@ -39,6 +39,7 @@
 
 #include "SIM_StratoBlimp.h"
 #include "SIM_Glider.h"
+#include "SIM_FlightAxis.h"
 
 extern const AP_HAL::HAL& hal;
 
@@ -1485,6 +1486,12 @@ const AP_Param::GroupInfo SIM::ModelParm::var_info[] = {
     // @Group: SLUP_
     // @Path: ./SIM_SlungPayload.cpp
     AP_SUBGROUPINFO(slung_payload_sim, "SLUP_", 4, SIM::ModelParm, SlungPayloadSim),
+#endif
+
+#if AP_SIM_FLIGHTAXIS_ENABLED
+    // @Group: RFL_
+    // @Path: ./SIM_FlightAxis.cpp
+    AP_SUBGROUPPTR(flightaxis_ptr, "RFL_", 5, SIM::ModelParm, FlightAxis),
 #endif
 
     AP_GROUPEND