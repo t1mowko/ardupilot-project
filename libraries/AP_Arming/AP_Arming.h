SShink0/Ardupilot

AP_Arming: Record arming time and add accessor for it

libraries/AP_Arming/AP_Arming.h
@@ -97,6 +97,9 @@ public:
     bool is_armed() const;
     bool is_armed_and_safety_off() const;
 
+    // Returns the time since boot (in millis) that we armed. Returns 0 if disarmed.
+    uint32_t arm_time_ms() const { return is_armed() ? last_arm_time_ms : 0; }
+
     // get bitmask of enabled checks
     uint32_t get_enabled_checks() const;
 
@@ -300,6 +303,8 @@ private:
     // vehicle has been disarmed at least once.
     Method _last_disarm_method = Method::UNKNOWN;
     Method _last_arm_method = Method::UNKNOWN;
+
+    uint32_t last_arm_time_ms; // last time we successfully armed
 
     uint32_t last_prearm_display_ms;  // last time we send statustexts for prearm failures
     bool running_arming_checks;  // true if the arming checks currently being performed are being done because the vehicle is trying to arm the vehicle

t1mowko/ardupilot-project

libraries/AP_Arming/AP_Arming.h
@@ -101,6 +101,9 @@ public:
     bool is_armed() const;
     bool is_armed_and_safety_off() const;
 
+    // Returns the time since boot (in microseconds) that we armed. Returns 0 if disarmed.
+    uint64_t arm_time_us() const { return is_armed() ? last_arm_time_us : 0; }
+
     // get bitmask of enabled checks
     uint32_t get_enabled_checks() const;
 
@@ -316,6 +319,8 @@ private:
     // vehicle has been disarmed at least once.
     Method _last_disarm_method = Method::UNKNOWN;
     Method _last_arm_method = Method::UNKNOWN;
+
+    uint64_t last_arm_time_us; // last time we successfully armed
 
     uint32_t last_prearm_display_ms;  // last time we send statustexts for prearm failures
     bool running_arming_checks;  // true if the arming checks currently being performed are being done because the vehicle is trying to arm the vehicle